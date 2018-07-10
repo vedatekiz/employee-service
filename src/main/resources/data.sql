2da878ea1589d31a664feef0842f4341f4dce720
Ekiz Ali Vedat z003jsxt <vedat.ekiz@siemens.com>
2018-07-10 17:07:14 +0300

    initial data for hobby is added to data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+--CREATE HOBBIES
+
+insert into hobby (id, hobby) values (HIBERNATE_SEQUENCE.nextval, 'soccer');
+insert into hobby (id, hobby) values (HIBERNATE_SEQUENCE.nextval, 'music');
+insert into hobby (id, hobby) values (HIBERNATE_SEQUENCE.nextval, 'basketball');
+insert into hobby (id, hobby) values (HIBERNATE_SEQUENCE.nextval, 'literature');
+insert into hobby (id, hobby) values (HIBERNATE_SEQUENCE.nextval, 'motorcycle');
+insert into hobby (id, hobby) values (HIBERNATE_SEQUENCE.nextval, 'drama');
